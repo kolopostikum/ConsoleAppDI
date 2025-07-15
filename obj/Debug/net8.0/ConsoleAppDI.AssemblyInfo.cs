cdb8389121c519ae9389a3f5dc05e7626911525c
said <kerorogunso@mail.ru>
2025-07-15 15:45:36 +0300

    feat(analysis): зафиксированы предварительные и фактические оценки шагов проектирования
    - Добавлен артефакт 00b_engineering_estimations.md с исходными оценками времени на каждый из 10 шагов
    - Добавлен артефакт 00c_plan_vs_fact.md для отслеживания расхождений между планом и фактом
    - Зафиксированы ключевые отклонения и причины увеличения времени по шагам 1–4

--- obj/Debug/net8.0/ConsoleAppDI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleAppDI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppDI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7c6a346472b3702f63252b9cc8aa945de1d47c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cacf9d79960cf45b9bd4f8e43bc40486d8c939")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppDI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppDI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
